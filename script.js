f942d3a0a99a8e26ababc136120c9433293b6d0e
manosai <manosaikabaddi2005@gmail.com>
2025-03-15 13:31:42 +0530

    FINALCHANGES

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
+const mobileMenu = document.getElementById("mobile");
+const nav = document.getElementById("nav");
 
-if (bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.add('active');
-    })
-}
-
-if (close) {
-    close.addEventListener('click', () => {
-        nav.classList.remove('active');
-    })
-}
+mobileMenu.addEventListener("click", () => {
+    nav.style.display = nav.style.display === "flex" ? "none" : "flex";
+});
