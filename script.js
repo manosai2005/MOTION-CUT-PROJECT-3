d8a7f309bc39b09af8985675284efffe00c77e8b
manosai <manosaikabaddi2005@gmail.com>
2025-03-15 13:14:08 +0530

    small change

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const mobileMenu = document.getElementById("mobile");
-const nav = document.getElementById("nav");
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
 
-mobileMenu.addEventListener("click", () => {
-    nav.style.display = nav.style.display === "flex" ? "none" : "flex";
-});
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
